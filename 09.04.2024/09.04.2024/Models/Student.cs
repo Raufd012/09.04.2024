e1049f480f1ef42e452fc16605ee209debb6b9b2
Rauf <161576550+Raufd012@users.noreply.github.com>
2024-04-09 00:59:29 +0400

    initial commit

---
+++ 09.04.2024/09.04.2024/Models/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _09._04._2024.Models
+{
+    public interface ICodeAcademy
+    {
+        string CodeEmail { get; set; }
+        void GenerateEmail();
+    }
+
+    public class Student : ICodeAcademy
+    {
+        private static int Count = 0;
+
+        public int Id 
+        {
+            get;
+            private set; 
+        }
+        public string Name 
+        {
+            get; 
+        }
+        public string Surname 
+        {
+            get; 
+        }
+        public string CodeEmail 
+        {
+            get;
+            set; 
+        }
+
+        public Student(string name, string surname)
+        {
+            Id = ++Count;
+            Name = name;
+            Surname = surname;
+            GenerateEmail();
+        }
+
+        public static void CheckName()
+        {
+
+        }
+
+        public void GenerateEmail()
+        {
+            string emailName = $"{Name.ToLower()}.{Surname.ToLower()}{Id}";
+            emailName = Regex.Replace(emailName, @"[^a-zA-Z0-9]", "");
+            CodeEmail = $"{emailName}@code.edu.az";
+        }
+    }
+}
---
+++ 09.04.2024/09.04.2024/Program.cs
@@ -0,0 +1,16 @@
+﻿using _09._04._2024.Models;
+
+namespace _09._04._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                Student student = new Student("Murad", "Aliyev");
+                Console.WriteLine($"Student Id: {student.Id}, Email: {student.CodeEmail}");
+            }
+        }
+    }
+}
